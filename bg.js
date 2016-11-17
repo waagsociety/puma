1837f4019c231d726bc39bcb7565d5952655d1db
Marcart22 <17282@ma-web.nl>
2016-11-17 13:41:53 +0100

    Add files via upload

---
+++ bg.js
@@ -0,0 +1,4 @@
+theButton.onclick = function pictureChange()
+{
+   document.getElementById("theImage").src="http://31.media.tumblr.com/fca646cd8fe87906e605ad7e8d039903/tumblr_mmoz4fWT6U1soh1p8o1_500.png";
+}
